bd14ce580cf3ae9398e95f863ef173f451b7fcd2
LDSrobotique <73029102+LDSrobotique@users.noreply.github.com>
2020-10-24 20:44:48 +0200

    Mise à jour test.ts

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
 // les tests vont ici ; cela ne sera pas compilé si ce paquet est utilisé en tant qu'extension.
 Ks0426.initialisation()
-Ks0426.avancer(50)
+Ks0426.tournerD(50)
 basic.pause(500)
 Ks0426.stopper()
 basic.pause(500)
-Ks0426.avancer(-50)
+Ks0426.tournerG(50)
 basic.pause(500)
 Ks0426.stopper()
