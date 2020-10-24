848e4daaaaa73ef4e8ec2666cdf3da71d8ab8539
LDSrobotique <73029102+LDSrobotique@users.noreply.github.com>
2020-10-24 16:59:38 +0200

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -25,8 +25,8 @@ export function initialisation (): void {
 // Si la vitesse est positive alors la roue tourne pour avancer sinon la roue tourne en sens inverse.
 //% blockId=Ks0426vitesseRG
 //% block="roue gauche à $vitesse"
-export function vitesse_roue_gauche (vitesse: number): void {
-    vitesse |= 50
+export function vitesse_roue_gauche (vitesse: number = 50): void {
+    vitesse |= 0
     vitesseRG = vitesse
     if (vitesseRG >= 0) {
         if (vitesseRG > 100) {
