c916efbf2adc036cf1600dd8375989859d728999
LDSrobotique <73029102+LDSrobotique@users.noreply.github.com>
2020-10-25 16:12:03 +0100

    Mise à jour test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,1 @@
 // les tests vont ici ; cela ne sera pas compilé si ce paquet est utilisé en tant qu'extension.
-Ks0426.initialisation()
-if (Ks0426.obstacleF()) {
-    if (!(Ks0426.obstacleD())) {Ks0426.tournerD(50)} else {Ks0426.tournerG(50)}
-} else {Ks0426.avancer(50)}
